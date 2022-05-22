f81d514e5344f6af2d90e890bbd981c26f25dd10
SungJoon Kwon <43159901+Kwon-Sung-joon@users.noreply.github.com>
2022-05-22 19:43:13 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,84 +4,95 @@ import json
 import os
 import time
 
-
 '''
 Create by sj kwon
 Email kofdx7@gmail.com
-
 '''
-def get_rds_status(rds_identifier):
-	rds_client = boto3.client('rds');
-	response = rds_client.describe_db_instances(DBInstanceIdentifier=rds_identifier);
-	rds_instances=response.get("DBInstances")
-	rds=rds_instances.pop(0)
-	rds_status=rds.get("DBInstanceStatus")
-	return rds_status
 
-def get_cron_pattern(_datetime):
-	cron_pattern = "cron(0 "  + _datetime.strftime("%H %d %m").strip() + " ? " + _datetime.strftime("%Y") + ")"
-	return cron_pattern
+class Rds:
+	def __init__(self,rds_identifier):
+		self.rds_client=boto3.client('rds');
+		self.rds_identifier=rds_identifier;
+		
+	def get_rds_status(self):
+		response = self.rds_client.describe_db_instances(DBInstanceIdentifier=self.rds_identifier);
+		rds_instances=response.get("DBInstances")
+		rds=rds_instances.pop(0)
+		rds_status=rds.get("DBInstanceStatus")
+		return rds_status
 
-def get_start_pattern(rds_status,rds_identifier):
-	if rds_status=="available":
-		print("# STOP RDS ...! ")
-		stop_rds(rds_identifier);
-	#set your time for rds start. 
+	def schedule_rds(self):
+		rds_status=self.get_rds_status();
+		cron_pattern=self.get_start_pattern(rds_status) if rds_status=="stopping" or rds_status=="available" else self.get_stop_pattern(rds_status);
+		return cron_pattern
+		
+	def stop_rds(self):
+		rds_snapshot = self.rds_identifier  + datetime.datetime.now().strftime("-%Y-%m-%d-%H%M").strip()
+		response = self.rds_client.stop_db_instance(
+	    		DBInstanceIdentifier=self.rds_identifier
+		)
+		print("#  SUCCESS RDS STOP")
+		
+	def start_rds(self):
+		response = self.rds_client.start_db_instance(
+    			DBInstanceIdentifier=self.rds_identifier
+			)
+		print("#  SUCCESS RDS START")	
+
+	def get_start_pattern(self,rds_status):
+		if rds_status=="available":
+			print("# STOP RDS ...! ")
+			self.stop_rds();
+		#set your time for rds start. 
 	#default is before 3 hours after 7 days
-	_datetime=datetime.datetime.today()+datetime.timedelta(days=7)-datetime.timedelta(hours=3)
-	cron_pattern=get_cron_pattern(_datetime);	
-	return ("START",cron_pattern)
-	
-def get_stop_pattern(rds_status,rds_identifier):
-	if rds_status == "stopped":
-		print("# START RDS ...! ")
-		start_rds(rds_identifier);
-	#set your time for rds stop. 
-	#default is 1 hours.
-	_datetime=datetime.datetime.today()+datetime.timedelta(hours=1)
-	cron_pattern=get_cron_pattern(_datetime);
-	return ("STOP",cron_pattern)
+		_datetime=datetime.datetime.today()+datetime.timedelta(days=7)-datetime.timedelta(hours=3)
+		cron_pattern=get_cron_pattern(_datetime);	
+		return ("START",cron_pattern)
+		
+	def get_stop_pattern(self,rds_status):
+		if rds_status == "stopped":
+			print("# START RDS ...! ")
+			self.start_rds();
+		#set your time for rds stop. 
+		#default is 1 hours.
+		_datetime=datetime.datetime.today()+datetime.timedelta(minutes=15)
+		cron_pattern=get_cron_pattern(_datetime);
+		return ("STOP",cron_pattern)
 
-def schedule_rds(rds_identifier):
-	rds_status=get_rds_status(rds_identifier);
-	client = boto3.client('rds')
-	cron_pattern=get_start_pattern(rds_status,rds_identifier) if rds_status=="stopping" or rds_status=="available" else get_stop_pattern(rds_status,rds_identifier);
+
+class EventBridge:
+	
+	def __init__(self,events_rule_name):
+		self.eb_client = boto3.client('events');
+		self.events_rule_name=events_rule_name;
+
+	def put_rule(self,schedule):
+		response = self.eb_client.put_rule(
+			Name=self.events_rule_name,
+			ScheduleExpression=schedule[1],
+			State='ENABLED'
+		)
+		print("#  PUT RULE RDS "+ schedule[0]+":" + self.events_rule_name + "[" + schedule[1] + "]")
+		
+
+
+
+			
+def get_cron_pattern(_datetime):
+	cron_pattern = "cron("  + _datetime.strftime("%M %H %d %m").strip() + " ? " + _datetime.strftime("%Y") + ")"
 	return cron_pattern
 
-def stop_rds(rds_identifier):
-	client=boto3.client('rds');
-	rds_snapshot = rds_identifier  + datetime.datetime.now().strftime("-%Y-%m-%d-%H%M").strip()
-	response = client.stop_db_instance(
-    DBInstanceIdentifier=rds_identifier
-	)
-	
-	print("#  SUCCESS RDS STOP")
 
-def start_rds(rds_identifier):
-	client=boto3.client('rds');
-	response = client.start_db_instance(
-    DBInstanceIdentifier=rds_identifier
-	)
-	print("#  SUCCESS RDS START")	
 
-def put_rule(schedule,events_rule_name):
-	client = boto3.client('events')
-	response = client.put_rule(
-		Name=events_rule_name,
-		ScheduleExpression=schedule[1],
-		State='ENABLED'
-	)
-	print("#  PUT RULE RDS "+ schedule[0]+":" + events_rule_name + "[" + schedule[1] + "]")
 
 def lambda_handler(event, context):
 	# TODO implement
 
-	rds_identifier = os.environ['RDS_IDENTIFIER']
-	events_rule_name = os.environ['EVENT_RULE']
+	rds=Rds(os.getenv('RDS_IDENTIFIER'))
+	eb=EventBridge(os.getenv('EVENT_RULE'))
 
-	schedule=schedule_rds(rds_identifier);
-	put_rule(schedule,events_rule_name);
-
+	schedule=rds.schedule_rds();
+	eb.put_rule(schedule);
 	
 	return {
 		'statusCode': 200,
